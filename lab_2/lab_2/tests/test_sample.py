b96d222f52275895466a352e002717d36a67420a
Marcus Manos <marcusmanos@berkeley.edu>
2022-10-09 22:53:44 -0700

    updating issues with main.py

--- lab_2/lab_2/tests/test_sample.py
+++ lab_2/lab_2/tests/test_sample.py
@@ -1,5 +1,5 @@
 import sys
-sys.path.append('../')
+sys.path.append("../")
 from fastapi import FastAPI
 from fastapi.testclient import TestClient
 from src.main import app
@@ -38,18 +38,29 @@ def test_predict_good():
                                , "AveOccup": 2.55555556, "Latitude": 37.88, "Longitude": -122.23})
 
     assert response.status_code == 200  # status code we defined in main.py
-    assert response.json() == {"output(s)": [4.413388694639972]}
+    assert response.json() == {
+  "output": [
+    [
+      4.413388694639972
+    ]
+  ]
+}
 
 def test_predict_good_2():
-    data = {'MedInc': 8.3252, 'HouseAge': 41.0, 'AveRooms': 6.98412698, 'AveBedrms': 1.02380952, 'Population': 322.0
-        , 'AveOccup': 2.55555556, 'Latitude': 37.88, 'Longitude': -122.23}
+    data = {"MedInc": 8.3252, "HouseAge": 41.0, "AveRooms": 6.98412698, "AveBedrms": 1.02380952, "Population": 322.0
+        , "AveOccup": 2.55555556, "Latitude": 37.88, "Longitude": -122.23}
     response = client.post("/predict",
                            headers={"data": "Input"},
                            json=data)
-    print(response.json())
     assert response.status_code == 200  # status code we defined in main.py
 
-    assert response.json() == {"output(s)": [4.413388694639972]}
+    assert response.json() == {
+  "output": [
+    [
+      4.413388694639972
+    ]
+  ]
+}
 
 def test_predict_bad_feature():
     response = client.post("/predict",
@@ -57,10 +68,21 @@ def test_predict_bad_feature():
                                     "AveRooms": "Average number of Rooms", "AveBedrms": "Average number of Bedrooms"
                                , "Population": "Population of area", "AveOccup": "Avearge Occupancy",
                                     "Latitude": "Lattitude", "Longitude": "Longitude"},
-                           json={'HouseAge': 41.0, 'AveRooms': 6.98412698, 'AveBedrms': 1.02380952, 'Population': 322.0, 'AveOccup': 2.55555556
-                               , 'Latitude': 37.88, 'Longitude': -122.23})
+                           json={"HouseAge": 41.0, "AveRooms": 6.98412698, "AveBedrms": 1.02380952, "Population": 322.0, "AveOccup": 2.55555556
+                               , "Latitude": 37.88, "Longitude": -122.23})
     assert response.status_code == 422  # status code we defined in main.py
-    assert response.json() == {'detail': [{'loc': ['query', 'data'], 'msg': 'field required', 'type': 'value_error.missing'}]}
+    assert response.json() == {
+  "detail": [
+    {
+      "loc": [
+        "body",
+        "MedInc"
+      ],
+      "msg": "field required",
+      "type": "value_error.missing"
+    }
+  ]
+}
 
 def test_predict_bad_value():
     response = client.post("/predict",
@@ -68,10 +90,21 @@ def test_predict_bad_value():
                                     "AveRooms": "Average number of Rooms", "AveBedrms": "Average number of Bedrooms"
                                , "Population": "Population of area", "AveOccup": "Avearge Occupancy",
                                     "Latitude": "Lattitude", "Longitude": "Longitude"},
-                           json={'MedInc': -15551, 'HouseAge': 41.0, 'AveRooms': 6.98412698, 'AveBedrms': 1.02380952, 'Population': 322.0
-                               , 'AveOccup': 2.55555556, 'Latitude': 37.88, 'Longitude': -122.23})
+                           json={"MedInc": -15551, "HouseAge": 41.0, "AveRooms": 6.98412698, "AveBedrms": 1.02380952, "Population": 322.0
+                               , "AveOccup": 2.55555556, "Latitude": 37.88, "Longitude": -122.23})
     assert response.status_code == 422  # status code we defined in main.py
-    assert response.json() == {'detail': [{'loc': ['query', 'data'], 'msg': 'field required', 'type': 'value_error.missing'}]}
+    assert response.json() == {
+  "detail": [
+    {
+      "loc": [
+        "body",
+        "MedInc"
+      ],
+      "msg": "Must be positive",
+      "type": "value_error"
+    }
+  ]
+}
 
 def test_predict_multi_vals():
     response = client.post("/predict",
@@ -79,10 +112,29 @@ def test_predict_multi_vals():
                                     "AveRooms": "Average number of Rooms", "AveBedrms": "Average number of Bedrooms"
                                , "Population": "Population of area", "AveOccup": "Avearge Occupancy",
                                     "Latitude": "Lattitude", "Longitude": "Longitude"},
-                           json={'MedInc': -15551, 'Extra input': 989, 'HouseAge': 41.0, 'AveRooms': 6.98412698, 'AveBedrms': 1.02380952, 'Population': 322.0
-                               , 'AveOccup': 2.55555556, 'Latitude': 37.88, 'Longitude': -122.23})
+                           json={"MedInc": -15551, "Extra input": 989, "HouseAge": 41.0, "AveRooms": 6.98412698, "AveBedrms": 1.02380952, "Population": 322.0
+                               , "AveOccup": 2.55555556, "Latitude": 37.88, "Longitude": -122.23})
     assert response.status_code == 422  # status code we defined in main.py
-    assert response.json() == {'detail': [{'loc': ['query', 'data'], 'msg': 'field required', 'type': 'value_error.missing'}]}
+    assert response.json() == {
+  "detail": [
+    {
+      "loc": [
+        "body",
+        "MedInc"
+      ],
+      "msg": "Must be positive",
+      "type": "value_error"
+    },
+    {
+      "loc": [
+        "body",
+        "Extra input"
+      ],
+      "msg": "extra fields not permitted",
+      "type": "value_error.extra"
+    }
+  ]
+    }
 
 def test_health():
     response = client.get("/health")
